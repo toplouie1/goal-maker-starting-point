f9379f675447d58f73aefa326a6955c95fc6edd6
Nima sherpa <Toplouie78@gmail.com>
2021-11-24 15:07:29 -0500

    created 2 component in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import { Component } from "react";
 
-const App = () => {
-  return (
-    <div className="app">
-      <NavBar />
-    </div>
-  );
-};
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			mainList: "",
+		};
+	}
+
+	render() {
+		// it is checking the total of all completed list
+		let CompletedListTotal = () => {
+			return <div>Total Completed List : 0</div>;
+		};
+		let MainList = () => {
+			return (
+				<div>
+					<h2>Lists</h2>
+					<form>
+						<input
+							name="mainList"
+							value={this.state.mainList}
+							onInput={this.changeInput}
+						/>
+						<button>Add List</button>
+					</form>
+				</div>
+			);
+		};
+
+		return (
+			<div className="app">
+				<NavBar />
+				<CompletedListTotal />
+				<MainList />
+			</div>
+		);
+	}
+}
 
 export default App;
